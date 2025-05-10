f5b2dde8420a0f7ed02a00367746b59266ca5739
Aesan De Silva <aesanclyde.desilva@gmail.com>
2025-05-10 16:32:05 +0800

    feat: add delete and toggle task methods

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,8 +28,20 @@ public class TaskController {
     }
 
     @PostMapping
-    public String createTasks(@RequestParam String title){
+    public String createTask(@RequestParam String title){
         taskService.createTask(title);
         return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "tasks";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id){
+        taskService.toggleTask(id);
+        return "tasks";
     }
 }
